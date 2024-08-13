91bdcfb6a7eb6b5242e8207e028fbc83a202dd87
unknown <nateware777@gmail.com>
2024-08-13 14:06:30 +0300

    mr.robot api

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const characters ={
+    'elliot' : {
+        'outfit': 'black hoodie',
+        'condition': 'paranoia',
+        'speciality': 'hacking'
+},
+    'mr_robot' : {
+        'outfit': 'robot jacket',
+        'condition': 'stable',
+        'speciality': 'hacking'
+},
+     'tyrell' : {
+        'outfit': 'black suit',
+        'condition': 'scatterd',
+        'job': 'cybersecurity at E-Corp'
+},
+    'unknown' : {
+       'outfit': 'unknown',
+       'condition': 'unknown',
+       'job': 'unknown'
+}
+}
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/index.html')
+})
+
+
+app.get('/api/:name',(req,res)=>{
+    const charactersName = req.params.name.toLocaleLowerCase()
+    if( characters[charactersName] ){
+        res.json(characters[charactersName])
+    }else{
+        res.json(characters['unknown'])
+    }
+
+})
+
+app.listen(PORT , ()=>{
+    console.log(`The server is now running on port ${PORT}! Betta Go Catch It! `)
+})
